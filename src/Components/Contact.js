8031213e9682a58ff665a841e4ee56e6ddf2383e
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2022-01-24 03:19:20 +0530

    responsive added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,28 +1,36 @@
-import React from 'react'
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import EmailIcon from '@material-ui/icons/Email';
-import "./Contact.css"
+import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import EmailIcon from "@material-ui/icons/Email";
+import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
+import "./Contact.css";
 function Contact() {
-    return (
-        <div className="contact">
-            <h2 className="contact_head">Contact Me </h2>
-            <div className="contact_container">
-                <div className="contact_social">
-                    <h2 className="social_head">Socials</h2>
-                    <GitHubIcon fontSize="large" className="icons"/>
-                    <LinkedInIcon fontSize="large" className="icons" />
-                    <InstagramIcon fontSize="large" className="icons" />
-                    <EmailIcon fontSize="large" className="icons" />
-                </div>
-                <div className="conatct_address">
-                <h2 className="email_add">mohit725045.mk@gmail.com</h2>
-                <h2 className="email_add">New Delhi,India</h2>
-                </div>
-            </div>
+    const handlePDF=()=>{
+        window.open("https://drive.google.com/file/d/1CDOWVGhUh_ClIhLgMS71GFp4c6Vs6w6P/view?usp=sharing");
+    }
+  return (
+    <div className="contact">
+      <h2 className="contact_head">Contact Me </h2>
+      <div className="contact_container">
+        <div className="contact_social">
+          <h2 className="social_head">Socials</h2>
+          <GitHubIcon fontSize="large" className="icons" />
+          <LinkedInIcon fontSize="large" className="icons" />
+          <InstagramIcon fontSize="large" className="icons" />
+          <EmailIcon fontSize="large" className="icons" />
         </div>
-    )
+        <div className="conatct_address">
+          <h2 className="email_add">mohit725045.mk@gmail.com</h2>
+          <h2 className="email_add">New Delhi,India</h2>
+        </div>
+      </div>
+      <div className="contact_resume">
+        <h1>My Resume</h1>
+        <PictureAsPdfIcon className="resume_" onClick={handlePDF}/>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import { useState } from "react";
+import "./Header.css";
+import MenuIcon from '@material-ui/icons/Menu';
+import $ from "jquery";
 function Header() {
-    return (
-        <div className="header">
-            <h2 onClick={{}}>Projects</h2>
-            <h2 onClick={{}}>About Me</h2>
-            <h2 onClick={{}}>Contact</h2>
-        </div>
-    )
+  
+  $(window).resize(()=>{
+     //console.log(typeof($(window).width()));
+    if($(window).width()<500){
+        console.log($(window).width());
+        $(".header_con").slideUp()
+    }
+    
+
+  })
+  return (
+    <div className="header">
+      <div className="header_con">
+        <h2>Projects</h2>
+        <h2>About Me</h2>
+        <h2>Contact</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
